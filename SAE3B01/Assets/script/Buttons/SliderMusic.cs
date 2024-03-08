0dfd73d89a695f74d293fc8541f7a08b59330594
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2024-03-08 09:10:39 +0100

    Revert "Update SliderMusic.cs"
    This reverts commit 80da74019d99ebf7fc23e999f0abb9147d06b718.

--- SAE3B01/Assets/script/Buttons/SliderMusic.cs
+++ SAE3B01/Assets/script/Buttons/SliderMusic.cs
@@ -4,55 +4,39 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// Gère le curseur pour ajuster le volume de la musique.
-/// </summary>
 public class SliderMusic : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
 
-    /// <summary>
-    /// Méthode appelée au démarrage.
-    /// Vérifie si la clé "musicVolume" existe dans les préférences du joueur.
-    /// Si elle n'existe pas, initialise le volume à 1 et charge les préférences.
-    /// Sinon, charge simplement les préférences existantes.
-    /// </summary>
     private void Start()
     {
-        if (!PlayerPrefs.HasKey("musicVolume"))
+        if(!PlayerPrefs.HasKey("musicVolume"))
         {
             PlayerPrefs.SetFloat("musicVolume", 1);
             Load();
         }
+
         else
         {
             Load();
         }
+        
     }
 
-    /// <summary>
-    /// Modifie le volume de l'écouteur audio en fonction de la valeur du curseur.
-    /// Enregistre également la nouvelle valeur du volume.
-    /// </summary>
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
         Save();
     }
 
-    /// <summary>
-    /// Charge la valeur du volume depuis les préférences et l'applique au curseur.
-    /// </summary>
     private void Load()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
     }
 
-    /// <summary>
-    /// Enregistre la valeur actuelle du curseur dans les préférences.
-    /// </summary>
     private void Save()
     {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        PlayerPrefs.SetFloat("musicVolume",volumeSlider.value);
     }
 }
+

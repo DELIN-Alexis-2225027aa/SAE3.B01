0143f056b10d0bd631fdec81388e7ef80de29a34
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2024-03-08 09:11:08 +0100

    Revert "Update CameraFollow.cs"
    This reverts commit aed1447f925477ee8dda0c2a30a777e903e48125.

--- SAE3B01/Assets/script/CameraFollow.cs
+++ SAE3B01/Assets/script/CameraFollow.cs
@@ -21,13 +21,16 @@ public class SuiviCamera2D : MonoBehaviour
             cam.orthographicSize += 3f * Time.deltaTime;
         }
 
-        // Récupère la position actuelle de la caméra
+        // Récupere la position actuel de la caméra
         Vector3 positionActuelle = transform.position;
 
-        // Crée la nouvelle position de la caméra à partir de la position de l'objet à suivre
+        // Créer la nouvelle position de la caméra a partir de la position de l'objet a suivre
         Vector3 newPosition = new Vector3(objetASuivre.position.x, objetASuivre.position.y, positionActuelle.z);
 
         // Déplace la caméra vers la nouvelle position
         transform.position = Vector3.Lerp(positionActuelle, newPosition, 1000f);
+
+
     }
 }
+

aed1447f925477ee8dda0c2a30a777e903e48125
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2024-03-08 08:43:24 +0100

    Update CameraFollow.cs

--- SAE3B01/Assets/script/CameraFollow.cs
+++ SAE3B01/Assets/script/CameraFollow.cs
@@ -21,16 +21,13 @@ public class SuiviCamera2D : MonoBehaviour
             cam.orthographicSize += 3f * Time.deltaTime;
         }
 
-        // Récupere la position actuel de la caméra
+        // Récupère la position actuelle de la caméra
         Vector3 positionActuelle = transform.position;
 
-        // Créer la nouvelle position de la caméra a partir de la position de l'objet a suivre
+        // Crée la nouvelle position de la caméra à partir de la position de l'objet à suivre
         Vector3 newPosition = new Vector3(objetASuivre.position.x, objetASuivre.position.y, positionActuelle.z);
 
         // Déplace la caméra vers la nouvelle position
         transform.position = Vector3.Lerp(positionActuelle, newPosition, 1000f);
-
-
     }
 }
-

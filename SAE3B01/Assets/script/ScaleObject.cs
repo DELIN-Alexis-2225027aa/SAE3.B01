35202ff214a214c51f4e4daea1d4283c2ce39b4a
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2024-03-08 09:11:19 +0100

    Revert "Update ScaleObject.cs"
    This reverts commit 301f65c6b22ef57a7af18903a5997af4fa523023.

--- SAE3B01/Assets/script/ScaleObject.cs
+++ SAE3B01/Assets/script/ScaleObject.cs
@@ -2,22 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Redimensionne un objet RectTransform pour occuper un tiers de la largeur de l'écran.
-/// </summary>
 public class ScaleObject : MonoBehaviour
 {
     [SerializeField] private RectTransform m_RectTransform;
 
     void Start()
     {
-        // Récupère la largeur de l'écran
         float screenWidth = Screen.width;
-
-        // Calcule un tiers de la largeur de l'écran
         float thirdOfScreenWidth = screenWidth / 1.5f;
-
-        // Ajuste la taille de l'objet RectTransform en fonction du tiers de la largeur de l'écran
         m_RectTransform.sizeDelta = new Vector2(thirdOfScreenWidth, m_RectTransform.sizeDelta.y);
     }
 }
